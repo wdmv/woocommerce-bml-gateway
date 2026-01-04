32dc6980a8d1eb6494a134a8976e4f3a71aadd93
hussainabeer <hussain.abeer2593@gmail.com>
2026-01-04 19:33:13 +0500

    updated version

--- plugin/bml-gateway-by-wdm.php
+++ plugin/bml-gateway-by-wdm.php
@@ -25,7 +25,7 @@
 defined('ABSPATH') || exit;
 
 // Define plugin constants.
-define('BML_GATEWAY_VERSION', '1.0.0');
+define('BML_GATEWAY_VERSION', '1.0.1');
 define('BML_GATEWAY_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('BML_GATEWAY_PLUGIN_URL', plugin_dir_url(__FILE__));
 define('BML_GATEWAY_PLUGIN_BASENAME', plugin_basename(__FILE__));
--- plugin/readme.txt
+++ plugin/readme.txt
@@ -6,7 +6,7 @@ Tested up to: 6.9
 Requires PHP: 7.4
 WC requires at least: 6.0
 WC tested up to: 9.3
-Stable tag: 1.0.0
+Stable tag: 1.0.1
 License: GPLv2 or later
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 
